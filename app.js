e59d163dd39c2ee0dcff2b972a00ee536a86f0e3
Shashank Shekhar <shekhar3@buffalo.edu>
2018-12-27 13:16:44 -0500

    Updates

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 const express = require('express');
-const path = require('path'); 
+const path = require('path');
 const chalk = require('chalk'); // To color highlight the log
 const flash = require('express-flash'); // for displaying error messages
 const session = require('express-session'); //session management
 const sass = require('node-sass-middleware');
+var mongoose = require('mongoose')
+var passport = require('passport')
+var configDB = require('./config/database.js');
+var sessions = require('sessions');
+/**
+ * Create Express server.
+ */
+const app = express();
+
+//configuration =================================
+mongoose.connect(configDB.url);
+require('./config/passport')(passport);
+
 
 /**
  * Controllers (route handlers).
  */
 const homeController = require('./controllers/home');
-const userController = require('./controllers/user');
+const userController = require('./controllers/users.js');
+const signupController = require('./controllers/signup');
 
 
-/**
- * Create Express server.
- */
-const app = express();
+
 
 /**
  * Express configuration.
@@ -24,10 +35,12 @@ app.set('port', 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(flash());
-app.use(session({
+//passport
+app.use(passport.initialize());
+app.use(passport.session({
     resave: true,
     saveUninitialized: true,
-    secret: 'adfvsvsdv',//process.env.SESSION_SECRET,
+    secret: 'adfvsvsdv', //process.env.SESSION_SECRET,
     cookie: { maxAge: 1209600000 } // two weeks in milliseconds
     // store: new MongoStore({
     //   url: process.env.MONGODB_URI,
@@ -35,8 +48,8 @@ app.use(session({
     // })
 }));
 app.use(sass({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public')
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public')
 }));
 app.use('/', express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
 app.use('/js/lib', express.static(path.join(__dirname, 'node_modules/popper.js/dist/umd'), { maxAge: 31557600000 }));
@@ -44,11 +57,16 @@ app.use('/js/lib', express.static(path.join(__dirname, 'node_modules/bootstrap/d
 app.use('/js/lib', express.static(path.join(__dirname, 'node_modules/jquery/dist'), { maxAge: 31557600000 }));
 app.use('/webfonts', express.static(path.join(__dirname, 'node_modules/@fortawesome/fontawesome-free/webfonts'), { maxAge: 31557600000 }));
 
+
+
+
 /**
  * Primary app routes.
  */
 app.get('/', homeController.index);
 app.get('/login', userController.getLogin);
+//app.get('/signup', signupController.getSignup);
+//app.post('/signup', signupController.postSignup);
 // app.post('/login', userController.postLogin);
 // app.get('/logout', userController.logout);
 // app.get('/forgot', userController.getForgot);
@@ -65,4 +83,4 @@ app.get('/login', userController.getLogin);
 app.listen(app.get('port'), () => {
     console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env'));
     console.log('  Press CTRL-C to stop\n');
-  });
+});
