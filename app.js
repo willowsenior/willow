afd46909ade48e6aa273f4732d43f1ad2afae846
Shashank Shekhar <shekhar3@buffalo.edu>
2018-12-27 13:35:09 -0500

    update to app.js file

--- app.js
+++ app.js
@@ -4,29 +4,41 @@ const chalk = require('chalk'); // To color highlight the log
 const flash = require('express-flash'); // for displaying error messages
 const session = require('express-session'); //session management
 const sass = require('node-sass-middleware');
-var mongoose = require('mongoose')
-var passport = require('passport')
-var configDB = require('./config/database.js');
-var sessions = require('sessions');
-/**
- * Create Express server.
- */
-const app = express();
+const mongoose = require('mongoose');
+const passport = require('passport');
+const expressValidator = require('express-validator');
+const bodyParser = require('body-parser');
 
-//configuration =================================
-mongoose.connect(configDB.url);
-require('./config/passport')(passport);
-
+const dburl = 'mongodb://shashank:shashank1234@ds245347.mlab.com:45347/willowtest'
 
 /**
  * Controllers (route handlers).
  */
 const homeController = require('./controllers/home');
-const userController = require('./controllers/users.js');
-const signupController = require('./controllers/signup');
+const userController = require('./controllers/user');
+
+// Passport Config
+const passportConfig = require('./config/passport');
 
 
+/**
+ * Create Express server.
+ */
+const app = express();
 
+
+/** Connect to database */
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useNewUrlparser', true);
+mongoose.connect(dburl, (err) => {
+    console.log('Database is connected')
+});
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+    console.log('MongoConnection Error Please check database connection', );
+    process.exit();
+})
 
 /**
  * Express configuration.
@@ -35,9 +47,10 @@ app.set('port', 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(flash());
-//passport
-app.use(passport.initialize());
-app.use(passport.session({
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressValidator());
+app.use(session({
     resave: true,
     saveUninitialized: true,
     secret: 'adfvsvsdv', //process.env.SESSION_SECRET,
@@ -47,6 +60,26 @@ app.use(passport.session({
     //   autoReconnect: true,
     // })
 }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+/////
+app.use((req, res, next) => {
+    // After successful login, redirect back to the intended page
+    if (!req.user &&
+        req.path !== '/login' &&
+        req.path !== '/signup' &&
+        !req.path.match(/^\/auth/) &&
+        !req.path.match(/\./)) {
+        req.session.returnTo = req.originalUrl;
+    } else if (req.user &&
+        (req.path === '/account' || req.path.match(/^\/api/))) {
+        req.session.returnTo = req.originalUrl;
+    }
+    next();
+});
+/////
 app.use(sass({
     src: path.join(__dirname, 'public'),
     dest: path.join(__dirname, 'public')
@@ -57,24 +90,19 @@ app.use('/js/lib', express.static(path.join(__dirname, 'node_modules/bootstrap/d
 app.use('/js/lib', express.static(path.join(__dirname, 'node_modules/jquery/dist'), { maxAge: 31557600000 }));
 app.use('/webfonts', express.static(path.join(__dirname, 'node_modules/@fortawesome/fontawesome-free/webfonts'), { maxAge: 31557600000 }));
 
-
-
-
 /**
  * Primary app routes.
  */
 app.get('/', homeController.index);
 app.get('/login', userController.getLogin);
-//app.get('/signup', signupController.getSignup);
-//app.post('/signup', signupController.postSignup);
 // app.post('/login', userController.postLogin);
 // app.get('/logout', userController.logout);
 // app.get('/forgot', userController.getForgot);
 // app.post('/forgot', userController.postForgot);
 // app.get('/reset/:token', userController.getReset);
 // app.post('/reset/:token', userController.postReset);
-// app.get('/signup', userController.getSignup);
-// app.post('/signup', userController.postSignup);
+app.get('/signup', userController.getSignup);
+app.post('/signup', userController.postSignup);
 
 
 /**
